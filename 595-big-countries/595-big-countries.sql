9e7d513e2600240ba955e03d89d229a32528dece
송원석 <dnjstjr0517@gmail.com>
2022-08-15 00:10:54 +0900

    Time: 391 ms (34.20%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -1,3 +1,16 @@
-SELECT name, population, area
-FROM World
-Where area >= 3000000 or population >= 25000000;
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area >= 3000000
+
+UNION
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population >= 25000000
+;
