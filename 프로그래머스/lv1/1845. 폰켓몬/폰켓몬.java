71acf866364daa84d74df5f25a89392c63ad9a9b
송원석 <dnjstjr0517@gmail.com>
2022-08-26 20:24:19 +0900

    [level 1] Title: 폰켓몬, Time: 1.28 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashSet<Integer> set = new HashSet();
+        
+        for(int num : nums) {
+            set.add(num);
+        }
+        
+        if(set.size() >= nums.length / 2) {
+            return nums.length / 2;
+        }
+        
+        return set.size();
+    }
+}
