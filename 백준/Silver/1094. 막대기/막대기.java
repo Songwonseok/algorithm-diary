7346a37460665bdddb9093ac6db8cf4f2c82507b
송원석 <dnjstjr0517@gmail.com>
2022-08-26 19:45:58 +0900

    [Silver V] Title: 막대기, Time: 128 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+		
+		
+		for(int i=0;i<7;i++) {
+			if((N & (1<<i)) > 0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+}
