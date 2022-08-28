b50aaa21b9a0e80614692af2d5b4de6e36436a26
송원석 <dnjstjr0517@gmail.com>
2022-08-29 01:08:17 +0900

    [level 2] Title: 올바른 괄호, Time: 16.35 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push('(');
+                continue;
+            }
+            
+            if(stack.isEmpty()) {
+                return false;
+            }
+            
+            stack.pop();
+        }
+        
+
+        return stack.isEmpty();
+    }
+}
