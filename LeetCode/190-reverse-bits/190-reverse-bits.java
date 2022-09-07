aa2f4e41c3596feb496cdf231706390165ffc978
송원석 <dnjstjr0517@gmail.com>
2022-09-07 19:45:09 +0900

    Time: 2 ms (29.16%), Space: 41.4 MB (98.72%) - LeetHub

--- LeetCode/190-reverse-bits/190-reverse-bits.java
+++ LeetCode/190-reverse-bits/190-reverse-bits.java
@@ -1,18 +1,20 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        StringBuilder sb = new StringBuilder();
+//         StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i <= 31; i++) {
-            int binary = 1 << i;
+//         for(int i = 0; i <= 31; i++) {
+//             int binary = 1 << i;
             
-            if((n & binary) == binary) {
-                sb.append("1");
-            }else {
-                sb.append("0");
-            }
-        }
+//             if((n & binary) == binary) {
+//                 sb.append("1");
+//             }else {
+//                 sb.append("0");
+//             }
+//         }
         
-        return (int)Long.parseLong(sb.toString(), 2);
+//         return (int)Long.parseLong(sb.toString(), 2);
+        
+        return Integer.reverse(n);
     }
 }
