0cf905830f7cebbe63b7b30c95f586b67233e848
송원석 <dnjstjr0517@gmail.com>
2022-09-07 19:52:13 +0900

    Time: 1 ms (100.00%), Space: 49.2 MB (85.48%) - LeetHub

--- LeetCode/136-single-number/136-single-number.java
+++ LeetCode/136-single-number/136-single-number.java
@@ -1,23 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet();
-        
-        for(int i = 0; i < nums.length; i++) {
-            if(set.contains(nums[i])) {
-                set.remove(nums[i]);
-                continue;
-            }
-            
-            set.add(nums[i]);
-        }
-        
+        // XOR 사용
         int answer = 0;
         
-        for(int a : set) {
-            answer = a;
+        for (int i = 0; i < nums.length; i++) {
+            answer ^= nums[i]; 
         }
         
         return answer;
-        
     }
 }
