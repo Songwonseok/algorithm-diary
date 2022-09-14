c0d94a0239adb06429b692f7b4da07cf57c5fecb
송원석 <dnjstjr0517@gmail.com>
2022-09-14 16:31:34 +0900

    [level 2] Title: 더 맵게, Time: 1603.70 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -1,22 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> pq = new PriorityQueue();
         
-        for(int i=0;i<scoville.length;i++) {
-        	pq.add(scoville[i]);
+        for(int s : scoville) {
+            pq.add(s);
         }
         
-        while(pq.peek() < K && pq.size() > 1) {
-        	int first = pq.poll();
-        	int second= pq.poll();
-        	
-        	pq.add(first+(second*2));
-        	answer++;
+        while(pq.size() >= 2 && pq.peek() < K) {
+            answer++;
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            pq.add(first + (second * 2));
         }
-        if(pq.peek() < K)
-        	return -1;
+        
+        if(pq.peek() < K) {
+            return -1;
+        }
         
         return answer;
     }
