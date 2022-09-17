3c7b3f18e0ef01945f142d697e0c3b7b0db558d8
송원석 <dnjstjr0517@gmail.com>
2022-09-17 11:01:03 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.25 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,9 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
-		int pivot = 0;
+		int offset = 0;
 
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+		Arrays.sort(jobs, (Comparator<int[]>)(o1, o2) -> {
 			if (o1[0] == o2[0]) {
 				return Integer.compare(o1[1], o2[1]);
 			}
@@ -13,34 +13,27 @@ class Solution {
 			return Integer.compare(o1[0], o2[0]);
 		});
 
+		PriorityQueue<int[]> pq = new PriorityQueue((Comparator<int[]>)(o1, o2) -> Integer.compare(o1[1], o2[1]));
 
-		for (int[] job : jobs) {
-			pq.add(job);
-		}
+		int index = 0;
 
-		while (!pq.isEmpty()) {
-			int[] job = pq.poll();
-            
-            if(pivot < job[0]) {
-                pivot = job[0];
-            }
-            
-			pivot += job[1];
-			answer += pivot - job[0];
-
-			int finalPivot = pivot;
-			PriorityQueue<int[]> temp = new PriorityQueue((Comparator<int[]>)(o1, o2) -> {
-				if (o1[0] <= finalPivot && o2[0] <= finalPivot) {
-					return Integer.compare(o1[1], o2[1]);
-				}
-
-				return Integer.compare(o1[0], o2[0]);
-			});
-
-			while (!pq.isEmpty()) {
-				temp.add(pq.poll());
+		while(true) {
+			while(index < jobs.length && jobs[index][0] <= offset) {
+				pq.offer(jobs[index++]);
 			}
-			pq = temp;
+            
+            if(pq.size() == 0) {
+                offset = jobs[index][0];
+                continue;
+            }
+
+			int[] job = pq.poll();
+            offset += job[1];
+            answer += offset - job[0];
+            
+            if(index == jobs.length && pq.size() == 0){
+                break;
+            }
 		}
 
 		return answer / jobs.length;
