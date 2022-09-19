59f5798e0670f59d430ed037d1b6a6d8f98f4bfb
송원석 <dnjstjr0517@gmail.com>
2022-09-20 00:20:34 +0900

    [level 2] Title: H-Index, Time: 0.40 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        Arrays.sort(citations);
+        
+        for(int i = 0; i < citations.length; i++) {
+            if(citations[i] > citations.length - i) {
+                answer = Math.max(citations.length - i, answer);
+                continue;
+            }
+            
+            answer = Math.max(citations[i], answer);
+        }
+        
+        return answer;
+    }
+}
