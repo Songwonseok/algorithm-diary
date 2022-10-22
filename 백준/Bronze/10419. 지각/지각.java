559e595ffae13a480c61c5a6e15febc2db0e6b96
송원석 <dnjstjr0517@gmail.com>
2022-10-22 22:54:24 +0900

    [Bronze III] Title: 지각, Time: 120 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/10419. 지각/지각.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for(int i = 0; i < T; i++) {
+			int d = Integer.parseInt(br.readLine());
+
+			int result = (int)Math.sqrt(d);
+
+			while(result > 0) {
+
+				if(d >= result + Math.pow(result, 2)) {
+					break;
+				}
+
+				result--;
+			}
+
+			System.out.println(result);
+		}
+	}
+}
