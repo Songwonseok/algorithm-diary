ca6829edb32d492df3a4acd8bae5704215fd262f
송원석 <dnjstjr0517@gmail.com>
2022-10-26 16:00:35 +0900

    [level 2] Title: 소수 찾기, Time: 71.76 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,89 +1,98 @@
-import java.math.*;
+import java.util.*;
+
 class Solution {
-    static boolean[] prime;
-    static boolean[] v, permCheck;
-    static int result;
-    static char[] selected,perm, number;
+    boolean[] isSelected, isPrimes;
+    HashSet<Integer> set;
+    int answer;
+    String numbers;
     
-    static void permutation(int d, int end){
-        if(d==end){
-            if(perm.length == 0)
-                return;
-            
-            StringBuilder str = new StringBuilder();
-            for(int i=0;i<perm.length;i++)
-                str.append(perm[i]);
-            
-            if(!prime[Integer.parseInt(str.toString())]) {
-            	prime[Integer.parseInt(str.toString())] = true;
-            	result++;
-            }
-            return;
-        }
-        
-        for(int i=0;i<selected.length;i++){
-            if(!permCheck[i]){
-                permCheck[i] = true;
-                perm[d] = selected[i];
-                permutation(d+1,end);
-                permCheck[i] = false;
-            }
-        }
-    }
-    
-    static void BP(int depth){
-        if(depth == number.length){
-            int cnt=0;
-            for(int i=0;i<depth;i++){
-                if(v[i])
-                    cnt++;
-            }
-            selected = new char[cnt];
-            perm = new char[cnt];
-            permCheck = new boolean[cnt];
-            
-            int index=0;
-            for(int i=0;i<depth;i++){
-                if(v[i])
-                    selected[index++] = number[i];
-            }
-            
-            permutation(0,cnt);
-            
-            return;
-        }
-        
-        v[depth] = true;
-        BP(depth+1);
-        v[depth] = false;
-        BP(depth+1);        
-    }
     public int solution(String numbers) {
-        int answer = 0;
-        result =0;
-        prime = new boolean[10000000];
-        number = new char[numbers.length()];
+        answer = 0;
         
+        isPrimes = getIsPrimes();
+        this.numbers = numbers;
+        isSelected = new boolean[numbers.length()];
+        set = new HashSet();
+        bruteForce(0);
         
-        
-        for(int i=0;i<number.length;i++)
-            number[i] = numbers.charAt(i);
-        
-        v = new boolean[number.length];
-        
-        prime[0] = true;
-        prime[1] = true;
-        
-        for(int i=2;i*i<10000000;i++){
-        	if(!prime[i]) {
-        		for(int j=i*i;j<10000000;j+=i){
-        			prime[j] = true;
-        		}
-        	}
-            
-        }
-        BP(0);
-        answer = result;
         return answer;
+    }
+    
+    public void perm(int depth, int permLength, char[] acc) {
+        if(depth == permLength) {
+            int value = Integer.parseInt(new String(acc));
+            
+            if(set.contains(value)) {
+                return;
+            }
+            
+            set.add(value);
+            
+            if(isPrimes[value]) {
+                answer++;
+            }
+            
+            return;
+        }
+        
+        for(int i = 0; i < isSelected.length; i++) {
+            if(isSelected[i]) {
+                if(depth == 0 && numbers.charAt(i) == '0') {
+                    continue;
+                }
+                
+                isSelected[i] = false;
+                acc[depth] = numbers.charAt(i);
+                perm(depth+1, permLength, acc);
+                isSelected[i] = true;
+            }
+        }
+        
+    }
+    
+    public void bruteForce(int depth) {
+        if(depth == isSelected.length) {
+            int permLength = 0;
+            
+            for(int i = 0; i < isSelected.length; i++) {
+                if(isSelected[i]) {
+                    permLength++;
+                }
+            }
+            
+            if(permLength == 0) {
+                return;
+            }
+            
+            char[] acc = new char[permLength];
+            
+            perm(0, permLength, acc);
+            return;
+        }
+        bruteForce(depth + 1);
+        isSelected[depth] = true;
+        bruteForce(depth + 1);
+        isSelected[depth] = false;
+        
+    }
+    
+    public boolean[] getIsPrimes() {
+        int MAX_SIZE = 10000000;
+        boolean[] isPrimes = new boolean[MAX_SIZE]; // 최대 7자리까지 가능
+        
+        Arrays.fill(isPrimes , true);
+        isPrimes[0] = false;
+        isPrimes[1] = false;
+        
+        for(int i = 2; i * i < MAX_SIZE; i++) {
+            if(isPrimes[i]) {
+                for(int j = i * 2; j < MAX_SIZE; j += i) {
+                    isPrimes[j] = false;
+                }
+            }
+        }
+        
+        return isPrimes;
+        
     }
 }
