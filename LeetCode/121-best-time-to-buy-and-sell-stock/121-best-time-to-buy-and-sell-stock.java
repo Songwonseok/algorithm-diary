5872ce6eecf1b9376a89bfa5ffc05c5701a605e4
송원석 <dnjstjr0517@gmail.com>
2023-01-03 10:40:24 +0900

    Time: 2 ms (83.99%), Space: 76.4 MB (56.04%) - LeetHub

--- LeetCode/121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ LeetCode/121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,27 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int length = prices.length;
-        int[] min = new int[length];
-        int[] max = new int[length];
         
-        // min : 왼쪽에서부터 가장 최소값 누적
-        // max : 오른쪽부터 가장 최대값 누적
-        
-        min[0] = prices[0];
-        max[length - 1] = prices[length - 1];
-        
-        for(int i = 1; i < length; i++) {
-            min[i] = Math.min(prices[i], min[i-1]);
-        }
-        
-        for(int i = length -2; i >= 0; i--) {
-            max[i] = Math.max(prices[i], max[i + 1]);
-        }
-        
+        int minValue = Integer.MAX_VALUE;
         int answer = 0;
         
-        for(int i = 0; i < length; i++) {
-            answer = Math.max(max[i] - min[i], answer);
+        for(int price : prices) {
+            minValue = Math.min(minValue, price);
+            
+            answer = Math.max(price - minValue, answer);
         }
         
         return answer;
