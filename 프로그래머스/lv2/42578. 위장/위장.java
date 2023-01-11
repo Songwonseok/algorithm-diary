f0ddc63f253493fe4f87fd50bbebed491a6fade7
송원석 <dnjstjr0517@gmail.com>
2023-01-11 13:35:26 +0900

    [level 2] Title: 위장, Time: 0.09 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -4,16 +4,18 @@ class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap();
         
-        for(int i = 0; i < clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1) + 1);
+        for(String[] c : clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 1) + 1); // 안입는 경우도 있어서 default: 1
         }
         
         int answer = 1;
         
-        for(String kind : map.keySet()) {
-            answer *= map.get(kind);
+        for(String key: map.keySet()) {
+           answer *= map.get(key); 
         }
         
-        return answer - 1; // 아무것도 안입은 경우 -1
+        answer--; // 아무것도 안입은 경우는 제외
+        
+        return answer;
     }
 }
