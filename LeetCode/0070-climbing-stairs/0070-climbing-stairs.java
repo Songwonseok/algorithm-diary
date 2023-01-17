e171e15881dbba183655d2df29575f2f40448496
송원석 <dnjstjr0517@gmail.com>
2023-01-17 16:13:30 +0900

    Time: 0 ms (100%), Space: 39 MB (86.3%) - LeetHub

--- LeetCode/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,14 @@
 class Solution {
-    public int climb(int[] dp, int n) {
-        if(n > 0 && dp[n] != 0) {
-            return dp[n];
-        }
-
-        dp[n] = climb(dp, n - 1) + climb(dp, n - 2);
-
-        return dp[n];
-    }
-
+    
     public int climbStairs(int n) {
         int[] dp = new int[46];
         dp[1] = 1;
         dp[2] = 2;
 
-        return climb(dp, n);
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 }
