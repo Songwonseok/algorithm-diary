d815fa18b7706461f79aad407789d6b6beb3a1b8
송원석 <dnjstjr0517@gmail.com>
2025-09-12 19:24:58 +0900

    Time: 2 ms (98.59%), Space: 41.9 MB (68.84%) - LeetHub

---
+++ LeetCode/1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap();
+        Set<Integer> set = new HashSet();
+
+        for(int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for(int key :map.keySet()) { 
+            if(set.contains(map.get(key))) {
+                return false;
+            }
+
+            set.add(map.get(key));
+        }
+
+        return true;
+    }
+}
