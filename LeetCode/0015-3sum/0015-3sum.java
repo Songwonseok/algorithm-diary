5a65ccb76ac44b0e74dbb0afc0378427ccbe1101
송원석 <dnjstjr0517@gmail.com>
2025-10-03 13:41:26 +0900

    Time: 30 ms (71.25%), Space: 51.3 MB (93.02%) - LeetHub

---
+++ LeetCode/0015-3sum/0015-3sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> answer = new ArrayList();
+
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < n - 2; i ++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+
+            int target = -nums[i];
+
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while(left < right) {
+                int sum = nums[left] + nums[right];
+
+                if(sum == target) {
+                    answer.add(List.of(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+
+                    // skip duplicates
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return answer;
+
+    }
+}
