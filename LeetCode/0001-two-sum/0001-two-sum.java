062bd7b5492716e64d185e6e0c3c13c4f900ee9c
송원석 <dnjstjr0517@gmail.com>
2025-12-15 11:53:59 +0900

    Time: 2 ms (99.1%), Space: 47.3 MB (10.96%) - LeetHub

--- LeetCode/0001-two-sum/0001-two-sum.java
+++ LeetCode/0001-two-sum/0001-two-sum.java
@@ -1,22 +1,18 @@
-import java.util.*;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap();        
-        
+        Map<Integer, Integer> map = new HashMap();
         int[] answer = new int[2];
-        
+
         for(int i = 0; i < nums.length; i++) {
-            if(map.containsKey(target - nums[i])) {
-                answer[0] = map.get(target - nums[i]);
+            if(map.containsKey(nums[i])) {
+                answer[0] = map.get(nums[i]);
                 answer[1] = i;
                 break;
             }
-            
-            map.put(nums[i], i); 
+
+            map.put(target - nums[i], i);
         }
-        
+
         return answer;
-         
     }
 }
