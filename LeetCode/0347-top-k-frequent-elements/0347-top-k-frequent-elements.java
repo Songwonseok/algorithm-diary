fb29450c158e043abcc4d7b4c43fd4e490322d8f
송원석 <dnjstjr0517@gmail.com>
2025-12-29 22:55:38 +0900

    Time: 13 ms (79.42%), Space: 47.9 MB (18.76%) - LeetHub

--- LeetCode/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ LeetCode/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,19 +2,15 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap();
 
-        int max = 0;
-
         for(int num : nums) {
             int count = map.getOrDefault(num, 0) + 1;
 
             map.put(num, count);
-
-            max = Math.max(max, count);
         }
 
         // Heap or bucket List
-        List<Integer>[] list = new ArrayList[max + 1];
-
+        List<Integer>[] list = new ArrayList[nums.length + 1];
+        
         for(Integer key : map.keySet()) {
             int count = map.get(key);
 
@@ -27,19 +23,14 @@ class Solution {
 
         List<Integer> answer = new ArrayList();
 
-        for(int i = list.length - 1; i >= 0; i--) {
+        for(int i = list.length - 1; i > 0 && k > 0; i--) {
             
             if(list[i] == null) {
                 continue;
             }
 
-            for(int num : list[i]) {
-                answer.add(num);
-
-                if(answer.size() == k) {
-                    return answer.stream().mapToInt(v -> v).toArray();
-                }
-            }
+            answer.addAll(list[i]);
+            k -= list[i].size();
         }
 
         return answer.stream().mapToInt(v -> v).toArray();
