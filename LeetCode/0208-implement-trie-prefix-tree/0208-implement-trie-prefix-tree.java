7b1fe773c9608f32ec414c9425f26222352b9f88
송원석 <dnjstjr0517@gmail.com>
2026-08-14 18:50:21 +0900

    Time: 43 ms (13.32%), Space: 63 MB (12.63%) - LeetHub

--- LeetCode/0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ LeetCode/0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,68 +1,66 @@
 class Trie {
-    private final Node root;
-    
+    private Node root;
+
     public Trie() {
-        root = new Node();
+        root = new Node();   
     }
     
     public void insert(String word) {
-        Node node = root;
+        Node curr = root;
 
         for(char c : word.toCharArray()) {
-            if(!node.children.containsKey(c)) {
-                node.children.put(c, new Node(c));
+            if(!curr.children.containsKey(c)) {
+                curr.children.put(c, new Node(c));
             }
-
-            node = node.children.get(c);
+            
+            curr = curr.children.get(c);
         }
 
-        node.isWord = true;
+        curr.isLast = true;
     }
     
     public boolean search(String word) {
-        Node node = findNode(word);
+        Node last = findLast(word);
 
-        return node != null && node.isWord;
+        return last != null && last.isLast;
     }
     
     public boolean startsWith(String prefix) {
-        Node node = findNode(prefix);
+        Node last = findLast(prefix);
 
-        return node != null;
+        return last != null;
     }
 
-    private Node findNode(String word) {
-        Node node = root;
+    public Node findLast(String word) {
+        Node curr = root;
 
         for(char c : word.toCharArray()) {
-            if(!node.children.containsKey(c)) {
+            if(!curr.children.containsKey(c)) {
                 return null;
             }
-
-            node = node.children.get(c);
+            
+            curr = curr.children.get(c);
         }
 
-        return node;
-    }
+        return curr;
+    } 
 
     class Node {
-        boolean isWord;
         char val;
+        boolean isLast;
         Map<Character, Node> children;
-        
+
         Node() {
-            isWord = false;
+            isLast = false;
             children = new HashMap();
         }
 
         Node(char val) {
             this.val = val;
+            isLast = false;
             children = new HashMap();
-            isWord = false;
         }
-
     }
-
 }
 
 /**
