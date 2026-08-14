512cffc1fdca9f89860eccfcfc321e6d6f79dd04
송원석 <dnjstjr0517@gmail.com>
2026-08-14 13:49:26 +0900

    Time: 0 ms (100%), Space: 46.8 MB (74.71%) - LeetHub

--- LeetCode/0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ LeetCode/0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -15,13 +15,15 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null || root.val == val) {
-            return root;
+        if(root == null) {
+            return null;
         }
 
-        if(root.val > val) {
+        if(root.val == val) {
+            return root;
+        }else if(root.val > val) {
             return searchBST(root.left, val);
-        }else {
+        } else {
             return searchBST(root.right, val);
         }
     }
