0e3bc5269b1bf462d69d717857466f905f7eed5d
송원석 <dnjstjr0517@gmail.com>
2026-08-16 20:05:17 +0900

    Time: 15 ms (100%), Space: 59 MB (100%) - LeetHub

--- LeetCode/0146-lru-cache/0146-lru-cache.java
+++ LeetCode/0146-lru-cache/0146-lru-cache.java
@@ -1,54 +1,56 @@
 class LRUCache {
-    private final int capacity;
-    private final Map<Integer, Node> cache;
+    private int capacity;
+    private Node head;
+    private Node tail;
 
-    private final Node head;
-    private final Node tail;
+    private Map<Integer, Node> map;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        cache = new HashMap();
 
         head = new Node();
         tail = new Node();
+        map = new HashMap();
 
         head.next = tail;
         tail.prev = head;
     }
     
     public int get(int key) {
-        if(!cache.containsKey(key)) {
+        if(!map.containsKey(key)) {
             return -1;
         }
 
-        Node node = cache.get(key);
+        Node node = map.get(key);
+
         removeNode(node);
         addToHead(node);
 
-        return node.value;
+        return node.val;
     }
     
     public void put(int key, int value) {
         Node node;
 
-        if(!cache.containsKey(key)) {
-            node = new Node(key, value);
-            cache.put(key, node);
-        }else {
-            node = cache.get(key);
-            node.value = value;
+        if(map.containsKey(key)) {
+            node = map.get(key);
+            node.val = value;
+
             removeNode(node);
+        }else {
+            node = new Node(key, value);
+            map.put(key, node);
         }
+
 
         addToHead(node);
 
-        if(cache.size() > capacity) {
-            Node removeTarget = tail.prev;
-
-            removeNode(removeTarget);
-            cache.remove(removeTarget.key);
-        }
-
+        if(map.size() > capacity) {
+            Node removed = tail.prev;
+            
+            removeNode(removed);
+            map.remove(removed.key);         
+        }        
     }
 
     private void removeNode(Node node) {
@@ -58,31 +60,31 @@ class LRUCache {
         prev.next = next;
         next.prev = prev;
 
-        node.prev = null;
         node.next = null;
+        node.prev = null;
     }
 
     private void addToHead(Node node) {
         Node next = head.next;
 
-        node.next = next;
         node.prev = head;
+        node.next = next;
 
-        next.prev = node;
         head.next = node;
+        next.prev = node;
     }
 
-    static class Node {
-        int key;
-        int value;
-        Node prev;
+    class Node {
         Node next;
+        Node prev;
+        int val;
+        int key;
 
         Node() {}
 
-        Node(int key, int value) {
+        Node(int key, int val) {
             this.key = key;
-            this.value = value;
+            this.val = val;
         }
     }
 }
