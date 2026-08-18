ebe01341524991b5b0f99b8f0a2c539270b847d0
송원석 <dnjstjr0517@gmail.com>
2026-08-18 22:34:31 +0900

    Time: 0 ms (100%), Space: 44 MB (93.15%) - LeetHub

--- LeetCode/0206-reverse-linked-list/0206-reverse-linked-list.java
+++ LeetCode/0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -13,11 +13,13 @@ class Solution {
         ListNode prev = null;
         ListNode curr = head;
 
-        while(curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        while(curr!= null) {
+            ListNode next = curr.next; 
+
+            curr.next = prev; 
+
+            prev = curr; 
+            curr = next; 
         }
 
         return prev;
